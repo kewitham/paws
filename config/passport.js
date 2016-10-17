aa4bfbdeb825a15764a02e82f202d4bc18a54df4
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-10-17 12:06:00 -0700

    working out conflicting server.js issues. include port listener in server.js or elsewhere. Comments at bottom of server.js page for Katie's review.

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 var passport = require ('passport'),
-	LocalStrategy = require('passport-local').LocalStrategy,
+	// LocalStrategy = require('passport-local').LocalStrategy,
+	LocalStrategy = require('passport-local').Strategy;
+
 	db = require('../models')
 
 // Serialize Sessions
@@ -16,12 +18,27 @@ passport.deserializeUser(function(user, done){
 	});
 });
 
-//  for Authentication
-passport.use(new LocalStrategy{
-	function(username, password, done){
-		db.User.find({where: {id: user.id}}).success(function(user){
-			passwd = user ? user.password : ""
-			isMatch = db.user.validPassword (password, passwd, done, user)
-		});
-	}
-});
+//  for Authentication acc to Ccode tutorial
+// passport.use(new LocalStrategy({
+// 	function(username, password, done){
+// 		db.User.find({where: {id: user.id}}).success(function(user){
+// 			passwd = user ? user.password : ""
+// 			isMatch = db.user.validPassword (password, passwd, done, user)
+// 		});
+// 	}
+// })); 
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require ('express')
 	// , ks routes = require('./routes')
-	, path = require('express')
+	, path = require('path')
 	, bodyParser = require ('body-parser')
 	, cookieParser = require ('cookie-parser')
-	, app = express()
 	, user = require ('./controllers/user_controller')
 	, db = require ('./models')
 	, http = require ('http')
@@ -12,31 +11,51 @@ var express = require ('express')
 	, methodOverride= require('method-override');
 	// , bcrypt = require('bcrypt'); see user.js
 
-var application = require('./controllers/application')
+var application = require('./controllers/application_controller')
 	, home = require('./controllers/home_controller')
 	, index = require('./controllers/index_controller')
 	, user = require('./controllers/user_controller')
-	
 
 	// Above 4 variables = model controllers (rather than routes)
 
+// instantiate app:
+var app = express()
+ 
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
 
+// below for encryption
 SALT_WORK_FACTOR =12;
 
-app.use('/public', express.static(__dirname + '/public'));
+//allow sessions acc to cats, not sure needed
+// app.use(session({ secret: 'app', cookie: { maxAge: 60000 }}));
+// app.use(cookieParser());
 
-app.set('views', __dirname + '/views')
 
-app.set('port', process.env.PORT || 3003)
+//engine set up
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded ())
-app.use(express.bodyParser())
-app.use(express.cookieParser())
-app.use(express.session ({secret: 'frogsReveryWhereForSecurity'}))
-app.use(passport.initialize())
-app.use(passport.session())
+
+//set up handlebars
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'main'
+// }));
+// app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('port', process.env.PORT || 3003);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.session ({secret: 'frogsReveryWhereForSecurity'}));
+app.use(passport.initialize());
+app.use(passport.session({secret: 'frogsReveryWhereForSecurity'}));
+
 // ks app.use(app.router)
-
 app.use('/', application_controller);
 app.use('/home', home_controller);
 app.use('/index', index_controller);
@@ -72,11 +91,18 @@ app.get('/home', application.IsAuthenticated, home.homepage)
 // 		failureRedirect: '/'
 // 	})
 // ) replaced by lines 75-79
-app.post('/authenticate', passport.authenticate('local'), function(req, res){
- //  	successRedirect: '/home',
-	// failureRedirect: '/'
-	console.log("passport user", req.user);
-});
+// app.post('/authenticate', passport.authenticate('local'), function(req, res){
+//  //  	successRedirect: '/home',
+// 	// failureRedirect: '/'
+// 	console.log("passport user", req.user);
+// });
+
+app.post('/auth', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth' }));
+// app.post('/login',
+//   passport.authenticate('local', { successRedirect: '/',
+//                                    failureRedirect: '/login',
+//                                    failureFlash: true })
+// ); as per documentation
 
 app.get ('/logout', application.destroySession)
 app.get ('/signup', user.signUp)
@@ -102,4 +128,25 @@ db
 		};
 
 	});
+
+// ---------Hey Katie----------
+// In cats solution they have the following: 
+// // our module get's exported as app.
+// module.exports = app;
+
+
+// // Where's the listen? Open up bin/www, and read the comments.
+
+// ---------Hey Katie / Michael----------
+// In the migration solution you pull the models in ....
+// // ---------Hey Katie----------
+// // and we bring in our models folder. This brings in the model's object, as defined in index.js
+// var models  = require('./models');
+
+// // extract our sequelize connection from the models object, to avoid confusion
+// var sequelizeConnection = models.sequelize;  and the port listener: 
+// // app listens on port 3000
+// app.listen(3000, function(){
+// 	console.log("Listening on port 3000")
+// })
 
